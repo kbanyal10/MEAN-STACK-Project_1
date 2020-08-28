cef8a48f9316a83125875bcd03be39ea8ec52fd2
Kartikeya Banyal <kbanyal10@gmail.com>
2020-08-28 19:16:13 -0400

    Fifth Commit - MongoDB Connection added

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,47 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const Post = require("./models/post");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://Kart:nmQeyocxttsEN9qW@cluster0.9axed.mongodb.net/<dbname>?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("DataBase Connect!");
+  })
+  .catch(() => {
+    console.log("Not Connected!");
+  });
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    "Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
   res.setHeader(
-    "Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token"
+    "Access-Control-Allow-Headers",
+    "Origin, Content-Type, X-Auth-Token"
   );
   next();
 });
 
-app.use("/api/posts", (req, res, next) => {
+app.post("/api/posts", (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+
+  console.log(post);
+  res.status(201).json({
+    message: "Message Recieved Successfully!",
+  });
+});
+
+app.get("/api/posts", (req, res, next) => {
   const posts = [
     {
       id: "adasd2323",
