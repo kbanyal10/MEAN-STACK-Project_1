3b182ea353c8f345815f372b695abee30d0762da
Kartikeya Banyal <kbanyal10@gmail.com>
2020-09-01 19:34:38 -0400

    Eleventh Commit - Modifying Routes

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const Post = require("./models/post");
 const mongoose = require("mongoose");
+const postRoutes = require("./routes/posts");
 
 mongoose
   .connect(
@@ -30,54 +30,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  post.save().then((createdPost) => {
-    res.status(201).json({
-      message: "Message Recieved Successfully!",
-      postId: createdPost._id,
-    });
-  });
-});
-
-app.put("/api/posts/:id", (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content,
-  });
-  Post.updateOne({ _id: req.params.id }, post).then((result) => {
-    console.log(result);
-    res.status(200).json({ message: "Updated Successfully!" });
-  });
-});
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find().then((documents) => {
-    res.status(200).json({
-      message: "Hello",
-      posts: documents,
-    });
-  });
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then((result) => {
-    res.status(200).json({ message: "Post Deleted!" });
-  });
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-  Post.findById(req.params.id).then((post) => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({ message: "Post not found!" });
-    }
-  });
-});
+app.use("/api/posts", postRoutes);
 
 module.exports = app;
