2a6bb640cda9ad2d9aaf55b795f7ac22c45fd44f
Kartikeya Banyal <kbanyal10@gmail.com>
2020-09-01 19:19:36 -0400

    Tenth Commit - Editing Posts Works

--- backend/app.js
+++ backend/app.js
@@ -70,4 +70,14 @@ app.delete("/api/posts/:id", (req, res, next) => {
   });
 });
 
+app.get("/api/posts/:id", (req, res, next) => {
+  Post.findById(req.params.id).then((post) => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: "Post not found!" });
+    }
+  });
+});
+
 module.exports = app;
